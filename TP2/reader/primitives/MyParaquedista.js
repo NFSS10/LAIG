801359bbff1d02b529a4ff3e78623c44f1e1f10f
Nuno Silva <nuno_nfss96@hotmail.com>
2016-11-18 23:07:10 +0000

    cena em desenvolvimento

---
+++ TP2/reader/primitives/MyParaquedista.js
@@ -0,0 +1,97 @@
+/**
+* MyParaquedista
+* @constructor
+*/
+function MyParaquedista(scene) {
+	CGFobject.call(this,scene);
+
+	this.cilindro = new MyCylinder(this.scene,1,1,1,20,20);
+	this.tronco = new MyCylinder(this.scene,1.7,1.2,1,20,20);
+	this.cabeca = new MySphere(this.scene, 0.3, 30, 30);
+};
+
+MyParaquedista.prototype = Object.create(CGFobject.prototype);
+MyParaquedista.prototype.constructor = MyParaquedista;
+
+
+MyParaquedista.prototype.display = function()
+ {
+	//cabeca
+	this.scene.pushMatrix();
+	this.scene.translate(-0.8,1.3,0);
+	this.cabeca.display();
+	this.scene.popMatrix();
+	 
+	 
+	//bracos
+	this.scene.pushMatrix();
+	this.scene.translate(-0.8,1,0.3);
+	this.scene.rotate(75*(Math.PI / 180.0),1,0,0);
+	this.scene.scale(0.09,0.09,0.6);
+	this.cilindro.display();
+	this.scene.popMatrix();
+	
+	this.scene.pushMatrix();
+	this.scene.translate(-0.8,1,-0.3);
+	this.scene.rotate(105*(Math.PI / 180.0),1,0,0);
+	this.scene.scale(0.09,0.09,0.6);
+	this.cilindro.display();
+	this.scene.popMatrix();
+	
+	this.scene.pushMatrix();
+	this.scene.translate(-0.8,0.5,-0.6);
+	this.scene.rotate(-105*(Math.PI / 180.0),1,0,0);
+	this.scene.scale(0.09,0.09,0.6);
+	this.cilindro.display();
+	this.scene.popMatrix();
+	
+	/* 
+	this.scene.pushMatrix();
+	this.scene.translate(-0.8,1,0.3);
+	this.scene.rotate(90*(Math.PI / 180.0),0,1,0);
+	this.scene.scale(0.09,0.09,0.6);
+	this.cilindro.display();
+	this.scene.popMatrix();
+	 */
+	 
+	//pernas
+	this.scene.pushMatrix();
+	this.scene.translate(0,0,0.15);
+	this.scene.rotate(-90*(Math.PI / 180.0),0,1,0);
+	this.scene.scale(0.12,0.12,0.8);
+	this.cilindro.display();
+	this.scene.popMatrix();
+	
+	this.scene.pushMatrix();
+	this.scene.translate(0,0,-0.15);
+	this.scene.rotate(-90*(Math.PI / 180.0),0,1,0);
+	this.scene.scale(0.12,0.12,0.8);
+	this.cilindro.display();
+	this.scene.popMatrix();
+	//...
+	this.scene.pushMatrix();
+	this.scene.translate(0,-0.7,0.15);
+	this.scene.rotate(-90*(Math.PI / 180.0),1,0,0);
+	this.scene.scale(0.09,0.1,0.7);
+	this.cilindro.display();
+	this.scene.popMatrix();
+	
+	this.scene.pushMatrix();
+	this.scene.translate(0,-0.7,-0.15);
+	this.scene.rotate(-90*(Math.PI / 180.0),1,0,0);
+	this.scene.scale(0.09,0.1,0.7);
+	this.cilindro.display();
+	this.scene.popMatrix();
+	
+	
+	//tronco
+	this.scene.pushMatrix();
+	this.scene.translate(-0.8,0,0);
+	this.scene.rotate(-90*(Math.PI / 180.0),1,0,0);
+	this.scene.scale(0.17,0.2,1);
+	this.tronco.display();
+	this.scene.popMatrix();
+	
+	
+
+}
--- TP2/reader/primitives/Vehicle.js
+++ TP2/reader/primitives/Vehicle.js
@@ -4,29 +4,47 @@ function Vehicle(scene) {
 	 this.scene=scene;
 	
      this.controlvertexes = [];
-	 this.ponto1 = new Ponto3(0,0,0);
+	 
+	 this.ponto1 = new Ponto3(15,5,0);
 	 this.controlvertexes.push(this.ponto1);
-	 this.ponto2 = new Ponto3(-0.25,0,-0.5);
+	 this.ponto2 = new Ponto3(5,20,-5);
 	 this.controlvertexes.push(this.ponto2);
-	 this.ponto3 = new Ponto3(-0.5,0,-1);
+	 this.ponto3 = new Ponto3(5,40,-5);
 	 this.controlvertexes.push(this.ponto3);
-	 
-	 this.ponto4 = new Ponto3(0,0,0);
+	 this.ponto4 = new Ponto3(15,55,0);
 	 this.controlvertexes.push(this.ponto4);
-	 this.ponto5 = new Ponto3(0,0.6,-0.5);
+	 
+	 this.ponto5 = new Ponto3(15,5,0);
 	 this.controlvertexes.push(this.ponto5);
-	 this.ponto6 = new Ponto3(0,1,-1);
+	 this.ponto6 = new Ponto3(10,20,15);
 	 this.controlvertexes.push(this.ponto6);
-	 
-	 this.ponto7 = new Ponto3(0,0,0);
+	 this.ponto7 = new Ponto3(10,40,15);
 	 this.controlvertexes.push(this.ponto7);
-	 this.ponto8 = new Ponto3(0.25,0,-0.5);
+	 this.ponto8 = new Ponto3(15,55,0);
 	 this.controlvertexes.push(this.ponto8);
-	 this.ponto9 = new Ponto3(0.5,0,-1);
-	 this.controlvertexes.push(this.ponto9);
 	 
-	 this.bico= new Patch(this.scene,2,2,20,20,this.controlvertexes);
-	 this.cilindro= new MyCylinder(this.scene,0.45,0.8,9,20,20);
+	 this.ponto6 = new Ponto3(20,5,0);
+	 this.controlvertexes.push(this.ponto6);
+	 this.ponto7 = new Ponto3(25,20,15);
+	 this.controlvertexes.push(this.ponto7);
+	 this.ponto8 = new Ponto3(25,40,15);
+	 this.controlvertexes.push(this.ponto8);
+	 this.ponto9 = new Ponto3(20,55,0);
+	  this.controlvertexes.push(this.ponto9);
+	 
+	 this.ponto10 = new Ponto3(20,5,0);
+	 this.controlvertexes.push(this.ponto10);
+	 this.ponto11 = new Ponto3(30,20,4);
+	 this.controlvertexes.push(this.ponto11);
+	 this.ponto12 = new Ponto3(30,40,4);
+	 this.controlvertexes.push(this.ponto12);
+	 this.ponto13 = new Ponto3(20,55,0);
+	 this.controlvertexes.push(this.ponto13);
+	 
+	 this.paraquedas= new Patch(this.scene,3,3,20,20,this.controlvertexes);
+	 this.cabo= new MyCylinder(this.scene,0.01,0.01,2.75,20,20);
+	 this.cilindro= new MyCylinder(this.scene,1,1,1,30,30);
+	 this.paraquedista = new MyParaquedista(this.scene);
      
 };
 
@@ -35,21 +53,54 @@ Vehicle.prototype.constructor = Vehicle;
 
 Vehicle.prototype.display = function() {
 
-	this.scene.translate(0,0,5);
+	this.scene.pushMatrix();
+	this.scene.translate(-1.75,3,3);
+	this.scene.rotate(-90*(Math.PI / 180.0),1,0,0);
+	this.scene.scale(0.1,0.1,0.1);
+	this.paraquedas.display();
+	this.scene.popMatrix();
+	
 	
 	this.scene.pushMatrix();
-	this.bico.display();
+	this.scene.translate(0, 0.9, 0.7);
+	this.scene.rotate(-50*(Math.PI / 180.0),1,0,0);
+	this.scene.rotate(-5*(Math.PI / 180.0),0,1,0);
+	this.cabo.display();
 	this.scene.popMatrix();
 	
 	this.scene.pushMatrix();
-	this.scene.rotate(Math.PI, 0, 0, 1);
-	this.bico.display();
+	this.scene.translate(0, 0.9, 0.7);
+	this.scene.rotate(-50*(Math.PI / 180.0),1,0,0);
+	this.scene.rotate(5*(Math.PI / 180.0),0,1,0);
+	this.cabo.display();
 	this.scene.popMatrix();
 	
 	this.scene.pushMatrix();
-	this.scene.translate( 0, 0, -10);
+	this.scene.translate(0.25, 3, -2.45);
+	this.scene.rotate(50*(Math.PI / 180.0),1,0,0);
+	this.scene.rotate(-5*(Math.PI / 180.0),0,1,0);
+	this.cabo.display();
+	this.scene.popMatrix();
+	
+	this.scene.pushMatrix();
+	this.scene.translate(-0.25, 3, -2.45);
+	this.scene.rotate(50*(Math.PI / 180.0),1,0,0);
+	this.scene.rotate(5*(Math.PI / 180.0),0,1,0);
+	this.cabo.display();
+	this.scene.popMatrix();
+	
+	this.scene.pushMatrix();
+	this.scene.translate(0.05,0.47,0);
+	this.scene.rotate(-90*(Math.PI / 180.0),0,1,0);
+	this.scene.scale(0.83,0.83,0.5);
 	this.cilindro.display();
 	this.scene.popMatrix();
 	
+	
+	this.scene.pushMatrix();
+	this.scene.translate(1,0,0);
+	this.paraquedista.display();
+	this.scene.popMatrix();
+
 	
 };
