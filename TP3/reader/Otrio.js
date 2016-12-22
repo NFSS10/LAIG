48ca221cf3e2dad7497d9f4050848683ce551a6e
up201404022 <neoliator25@gmail.com>
2016-12-22 01:10:56 +0000

    Funções criadas mas nao funcionais

--- TP3/reader/Otrio.js
+++ TP3/reader/Otrio.js
@@ -36,23 +36,58 @@ Otrio.prototype.constructor=Otrio;
 
 Otrio.prototype.fazJogada = function()
 {
-  //Fazer a jogada
-  if(this.selectedPiece != null && this.posTomove != null)
+  if(this.selectedPiece != null)
   {
-    //TODO prolog
-    console.log("\nJogada:\n moveu peca:" + this.selectedPiece + " para pos: " + this.posTomove +"\n\n");
-
     if(this.playerTurn == 1)
     {
       res = this.possivelJogarVerm();
-      console.log("É possivel jogar vermelho" + res);
+      console.log("É possivel jogar essa peça" + res);
     }
     else if(this.playerTurn == 2)
     {
       res = this.possivelJogarAzul();
-      console.log("É possivel jogar Azul" + res);
+      console.log("É possivel jogar essa peça" + res);
     }
 
+  }
+  if(this.selectedPiece != null && this.posTomove != null)
+  {
+    var permitida=0;
+   
+    if(this.playerTurn == 1)
+    {
+      if(this.selectedPiece == 18 || this.selectedPiece == 21 || this.selectedPiece == 24)
+        permitida=  this.veriffazjogadaVermG();
+      else if(this.selectedPiece == 19 || this.selectedPiece == 22 || this.selectedPiece == 25)
+        permitida=  this.veriffazjogadaVermM();
+      else if(this.selectedPiece == 20 || this.selectedPiece == 23 || this.selectedPiece == 26)
+        permitida=  this.veriffazjogadaVermP();
+
+      console.log("ENTROOWWWWWWWWW "+ permitida);
+      if(permitida==1)
+      {
+        
+        this.fazjogadaVerm();
+      }
+    }
+    else if(this.playerTurn == 2)
+    {
+      if(this.selectedPiece == 9 || this.selectedPiece == 12 || this.selectedPiece == 15)
+         permitida= this.veriffazjogadaAzulG();
+      else if(this.selectedPiece == 10 || this.selectedPiece == 13 || this.selectedPiece == 16)
+         permitida= this.veriffazjogadaAzulM();
+      else if(this.selectedPiece == 11 || this.selectedPiece == 14 || this.selectedPiece == 17)
+         permitida= this.veriffazjogadaAzulP();
+      
+      console.log("ENTROOWWWWWWWWW "+ permitida);
+      if(permitida==1)
+      {
+        this.fazjogadaAzul();
+      }
+    }
+    console.log("\nJogada:\n moveu peca:" + this.selectedPiece + " para pos: " + this.posTomove +"\n\n");
+
+    
     this.changePlayer();
     this.reset_Seleccoes();
   }
@@ -191,3 +226,484 @@ this.engineResponse = null;
 
 return res;
 }
+
+
+Otrio.prototype.veriffazjogadaVermG = function()
+{
+   var game = this;
+   var res;
+   var str = "veriffazjogadaVermG";
+   var strPiece;
+
+   if(this.posTomove != null)
+   {
+      switch(this.posTomove) 
+      {
+          case 0:
+             strPiece= "(0,0)";
+            break;
+          case 1:
+             strPiece= "(1,0)";
+            break;
+          case 2:
+             strPiece= "(2,0)";
+            break;
+          case 3:
+             strPiece= "(0,1)";
+            break;
+          case 4:
+             strPiece= "(1,1)";
+            break;
+          case 5:
+             strPiece= "(2,1)";
+            break;
+          case 6:
+             strPiece= "(0,2)";
+            break;
+          case 7:
+             strPiece= "(1,2)";
+            break;
+          case 8:
+             strPiece= "(2,2)";
+            break;
+
+      }
+   }
+
+  str = str + strPiece;
+  console.log("Jogada vermelha grande verificada: " + strPiece);
+
+  //Verifica se é possivel jogar a peca
+  this.client.getPrologRequest(str, function(data) {
+    game.engineResponse = data.target.responseText;
+    });
+
+res = this.engineResponse;
+this.engineResponse = null;
+
+return res;
+}
+
+
+Otrio.prototype.veriffazjogadaVermM = function()
+{
+   var game = this;
+   var res;
+   var str = "veriffazjogadaVermM";
+   var strPiece;
+
+   if(this.posTomove != null)
+   {
+      switch(this.posTomove) 
+      {
+          case 0:
+             strPiece= "(0,0)";
+            break;
+          case 1:
+             strPiece= "(1,0)";
+            break;
+          case 2:
+             strPiece= "(2,0)";
+            break;
+          case 3:
+             strPiece= "(0,1)";
+            break;
+          case 4:
+             strPiece= "(1,1)";
+            break;
+          case 5:
+             strPiece= "(2,1)";
+            break;
+          case 6:
+             strPiece= "(0,2)";
+            break;
+          case 7:
+             strPiece= "(1,2)";
+            break;
+          case 8:
+             strPiece= "(2,2)";
+            break;
+
+      }
+   }
+
+  str = str + strPiece;
+  console.log("Jogada vermelha media verificada:  " + strPiece);
+
+  //Verifica se é possivel jogar a peca
+  this.client.getPrologRequest(str, function(data) {
+    game.engineResponse = data.target.responseText;
+    });
+
+res = this.engineResponse;
+this.engineResponse = null;
+
+return res;
+}
+
+
+Otrio.prototype.veriffazjogadaVermP = function()
+{
+   var game = this;
+   var res;
+   var str = "veriffazjogadaVermP";
+   var strPiece;
+
+   if(this.posTomove != null)
+   {
+      switch(this.posTomove) 
+      {
+          case 0:
+             strPiece= "(0,0)";
+            break;
+          case 1:
+             strPiece= "(1,0)";
+            break;
+          case 2:
+             strPiece= "(2,0)";
+            break;
+          case 3:
+             strPiece= "(0,1)";
+            break;
+          case 4:
+             strPiece= "(1,1)";
+            break;
+          case 5:
+             strPiece= "(2,1)";
+            break;
+          case 6:
+             strPiece= "(0,2)";
+            break;
+          case 7:
+             strPiece= "(1,2)";
+            break;
+          case 8:
+             strPiece= "(2,2)";
+            break;
+
+      }
+   }
+
+  str = str + strPiece;
+  console.log("Jogada vermelha pequena verificada: " + strPiece);
+
+  //Verifica se é possivel jogar a peca
+  this.client.getPrologRequest(str, function(data) {
+    game.engineResponse = data.target.responseText;
+    });
+
+res = this.engineResponse;
+this.engineResponse = null;
+
+return res;
+}
+
+
+
+Otrio.prototype.veriffazjogadaAzulG = function()
+{
+   var game = this;
+   var res;
+   var str = "veriffazjogadaAzulG";
+   var strPiece;
+
+   if(this.posTomove != null)
+   {
+      switch(this.posTomove) 
+      {
+          case 0:
+             strPiece= "(0,0)";
+            break;
+          case 1:
+             strPiece= "(1,0)";
+            break;
+          case 2:
+             strPiece= "(2,0)";
+            break;
+          case 3:
+             strPiece= "(0,1)";
+            break;
+          case 4:
+             strPiece= "(1,1)";
+            break;
+          case 5:
+             strPiece= "(2,1)";
+            break;
+          case 6:
+             strPiece= "(0,2)";
+            break;
+          case 7:
+             strPiece= "(1,2)";
+            break;
+          case 8:
+             strPiece= "(2,2)";
+            break;
+
+      }
+   }
+
+  str = str + strPiece;
+  console.log("Jogada azul grande verificada: " + strPiece);
+
+  //Verifica se é possivel jogar a peca
+  this.client.getPrologRequest(str, function(data) {
+    game.engineResponse = data.target.responseText;
+    });
+
+res = this.engineResponse;
+this.engineResponse = null;
+
+return res;
+}
+
+
+Otrio.prototype.veriffazjogadaAzulM = function()
+{
+   var game = this;
+   var res;
+   var str = "veriffazjogadaAzulM";
+   var strPiece;
+
+   if(this.posTomove != null)
+   {
+      switch(this.posTomove) 
+      {
+          case 0:
+             strPiece= "(0,0)";
+            break;
+          case 1:
+             strPiece= "(1,0)";
+            break;
+          case 2:
+             strPiece= "(2,0)";
+            break;
+          case 3:
+             strPiece= "(0,1)";
+            break;
+          case 4:
+             strPiece= "(1,1)";
+            break;
+          case 5:
+             strPiece= "(2,1)";
+            break;
+          case 6:
+             strPiece= "(0,2)";
+            break;
+          case 7:
+             strPiece= "(1,2)";
+            break;
+          case 8:
+             strPiece= "(2,2)";
+            break;
+
+      }
+   }
+
+  str = str + strPiece;
+  console.log("Jogada azul media verificada: " + strPiece);
+
+  //Verifica se é possivel jogar a peca
+  this.client.getPrologRequest(str, function(data) {
+    game.engineResponse = data.target.responseText;
+    });
+
+res = this.engineResponse;
+this.engineResponse = null;
+
+return res;
+}
+
+
+Otrio.prototype.veriffazjogadaAzulP = function()
+{
+   var game = this;
+   var res;
+   var str = "veriffazjogadaAzulP";
+   var strPiece;
+
+   if(this.posTomove != null)
+   {
+      switch(this.posTomove) 
+      {
+          case 0:
+             strPiece= "(0,0)";
+            break;
+          case 1:
+             strPiece= "(1,0)";
+            break;
+          case 2:
+             strPiece= "(2,0)";
+            break;
+          case 3:
+             strPiece= "(0,1)";
+            break;
+          case 4:
+             strPiece= "(1,1)";
+            break;
+          case 5:
+             strPiece= "(2,1)";
+            break;
+          case 6:
+             strPiece= "(0,2)";
+            break;
+          case 7:
+             strPiece= "(1,2)";
+            break;
+          case 8:
+             strPiece= "(2,2)";
+            break;
+
+      }
+   }
+
+  str = str + strPiece;
+  console.log("Jogada azul pequena verificada: " + strPiece);
+
+  //Verifica se é possivel jogar a peca
+  this.client.getPrologRequest(str, function(data) {
+    game.engineResponse = data.target.responseText;
+    });
+
+res = this.engineResponse;
+this.engineResponse = null;
+
+return res;
+}
+
+
+Otrio.prototype.fazjogadaVerm = function()
+{
+   var game = this;
+   var res;
+   var str = "fazjogadaVerm";
+   var strPiece;
+
+   if(this.posTomove != null && this.selectedPiece !=null)
+   {
+      switch(this.posTomove) 
+      {
+          case 0:
+             strPiece= "(0,0,";
+            break;
+          case 1:
+             strPiece= "(1,0,";
+            break;
+          case 2:
+             strPiece= "(2,0,";
+            break;
+          case 3:
+             strPiece= "(0,1,";
+            break;
+          case 4:
+             strPiece= "(1,1,";
+            break;
+          case 5:
+             strPiece= "(2,1,";
+            break;
+          case 6:
+             strPiece= "(0,2,";
+            break;
+          case 7:
+             strPiece= "(1,2,";
+            break;
+          case 8:
+             strPiece= "(2,2,";
+            break;
+
+      }
+
+      if(this.selectedPiece != null)
+      {
+        if(this.selectedPiece == 18 || this.selectedPiece == 21 || this.selectedPiece == 24)
+          strPiece = strPiece +  "r3)";
+        else if(this.selectedPiece == 19 || this.selectedPiece == 22 || this.selectedPiece == 25)
+          strPiece = strPiece +  "r2)";
+        else if(this.selectedPiece == 20 || this.selectedPiece == 23 || this.selectedPiece == 26)
+          strPiece = strPiece +  "r1)";
+      }
+
+   }
+
+  str = str + strPiece;
+  console.log("Jogada vermelha: " + strPiece);
+
+  //Verifica se é possivel jogar a peca
+  this.client.getPrologRequest(str, function(data) {
+    game.engineResponse = data.target.responseText;
+    });
+
+res = this.engineResponse;
+this.engineResponse = null;
+
+return res;
+}
+
+
+
+Otrio.prototype.fazjogadaAzul = function()
+{
+   var game = this;
+   var res;
+   var str = "fazjogadaAzul";
+   var strPiece;
+
+   if(this.posTomove != null && this.selectedPiece !=null)
+   {
+      switch(this.posTomove) 
+      {
+          case 0:
+             strPiece= "(0,0,";
+            break;
+          case 1:
+             strPiece= "(1,0,";
+            break;
+          case 2:
+             strPiece= "(2,0,";
+            break;
+          case 3:
+             strPiece= "(0,1,";
+            break;
+          case 4:
+             strPiece= "(1,1,";
+            break;
+          case 5:
+             strPiece= "(2,1,";
+            break;
+          case 6:
+             strPiece= "(0,2,";
+            break;
+          case 7:
+             strPiece= "(1,2,";
+            break;
+          case 8:
+             strPiece= "(2,2,";
+            break;
+
+      }
+      
+      if(this.selectedPiece != null)
+      {
+        if(this.selectedPiece == 9 || this.selectedPiece == 12 || this.selectedPiece == 15)
+          strPiece = "(b3)";
+        else if(this.selectedPiece == 10 || this.selectedPiece == 13 || this.selectedPiece == 16)
+          strPiece = "(b2)";
+        else if(this.selectedPiece == 11 || this.selectedPiece == 14 || this.selectedPiece == 17)
+          strPiece = "(b1)";
+      }
+
+   }
+
+  str = str + strPiece;
+  console.log("Jogada azul: " + strPiece);
+
+  //Verifica se é possivel jogar a peca
+  this.client.getPrologRequest(str, function(data) {
+    game.engineResponse = data.target.responseText;
+    });
+
+res = this.engineResponse;
+this.engineResponse = null;
+
+return res;
+}
+
